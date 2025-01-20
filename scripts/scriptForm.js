e9b0c98ad0e5dff47c0293db3b3f919878e2b543
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2025-01-20 19:10:37 +0100

    MEJORMIENTO EN CAMBIO DE COLOR PAGINA FORMULARIO

--- scripts/scriptForm.js
+++ scripts/scriptForm.js
@@ -2,15 +2,26 @@ let estaCambiado = false;
 
 document.getElementById('btn-color').addEventListener('click', function() {
     const mainElement = document.querySelector('main.contacto');
+    const texts = mainElement.querySelectorAll('legend, h2, p, label'); // Selección general de hijos relevantes
+    const actividadElement = mainElement.querySelector('td b'); // Selecciona específicamente el <b> dentro del <td>
     
     if (estaCambiado) {
-        // Vuelve al color inicial
         mainElement.style.backgroundColor = '';
-        mainElement.style.color = '';
+        texts.forEach(element => {
+            element.style.color = ''; // Restaura el color predeterminado
+        });
+        if (actividadElement) {
+            actividadElement.style.color = ''; // Restaura color específico del elemento
+        }
         estaCambiado = false;
     } else {
         mainElement.style.backgroundColor = '#1A535C';
-        mainElement.style.color = 'white';
+        texts.forEach(element => {
+            element.style.color = 'white'; // Cambia el color a blanco para los textos generales
+        });
+        if (actividadElement) {
+            actividadElement.style.color = 'white'; // FORZAR EL CAMBIO PQ NO TIENE <b> y poniendo label daba errores en accesibilidad 'ERR SIN SENTIDO'
+        }
         estaCambiado = true;
     }
 });
