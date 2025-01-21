4c51b674b2bd4f6613bbac3e625764f32471c0c2
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2025-01-21 09:35:04 +0100

    limpiar codigo nada mas

--- scripts/script.js
+++ scripts/script.js
@@ -8,15 +8,23 @@ const enviar = document.getElementById("enviar");
 let esta_cambiado = false;
 
 document.getElementById('btn-color').addEventListener('click', function() {
+    const divs = document.querySelectorAll('div:not(footer div):not(header div)'); // Excluye divs dentro de footer y header
     if (esta_cambiado) {
-        // Elimina el color volviendo al estado inicial
+        // Elimina los cambios volviendo al estado inicial
         document.body.style.background = '';
         document.body.style.color = '';
+        divs.forEach(div => {
+            div.style.border = ''; // Quita el color de borde
+        });
         esta_cambiado = false;
     } else {
-        // Aplica el color #1A535C
+        // Aplica los cambios
         document.body.style.background = '#1A535C';
         document.body.style.color = 'white';
+        divs.forEach(div => {
+            div.style.border = '2px solid white'; // Cambia el color de borde a blanco
+        });
         esta_cambiado = true;
     }
 });
+
